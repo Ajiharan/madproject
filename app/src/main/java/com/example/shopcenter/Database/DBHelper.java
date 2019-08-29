b5583ed08cdc6d35478808c6f301487971b564b7
Ajiharan <53114753+Ajiharan@users.noreply.github.com>
2019-08-29 08:10:57 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/shopcenter/Database/CustomerMaster.java
    #	app/src/main/java/com/example/shopcenter/Database/DBHelper.java
    #	app/src/main/res/values/strings.xml
    delete seller details

--- app/src/main/java/com/example/shopcenter/Database/DBHelper.java
+++ app/src/main/java/com/example/shopcenter/Database/DBHelper.java
@@ -9,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import com.example.shopcenter.model.CategoryItems;
+import com.example.shopcenter.model.Products;
 
 import java.util.ArrayList;
 
@@ -42,9 +43,21 @@ public class DBHelper extends SQLiteOpenHelper {
                 CustomerMaster.ProductCategory.COLUMN_NAME_IMAGE+" LONGBLOB,"+
                 CustomerMaster.ProductCategory.COLIMN_NAME_CATEGORY_NAME+" TEXT)";
 
+        String ADMIN_PRODUCT_DETAILS_ENTRIES="CREATE TABLE "+ CustomerMaster.ProductItems.TABLE_NAME +"("+
+                CustomerMaster.ProductItems.COLUMN_NAME_ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_PRODUCT_NAME + " TEXT,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_COUNT +" INTEGER,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_DESCRIPTION +" TEXT,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_PRICE + " TEXT,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_PRODUCTIMAGE + " LONGBLOB,"+
+                CustomerMaster.ProductItems.COLUMN_NAME_FOREIGNKEY+" INTEGER,CONSTRAINT fk_pro_cat FOREIGN KEY ("+
+                CustomerMaster.ProductItems.COLUMN_NAME_FOREIGNKEY + ") REFERENCES "+ CustomerMaster.ProductCategory.TABLE_NAME+"("+
+                CustomerMaster.ProductCategory.COLUMN_NAME_ID +") ON DELETE CASCADE ON UPDATE CASCADE)";
+
         sqLiteDatabase.execSQL(CUSTOMER_CREATE_ENTRIES);
         sqLiteDatabase.execSQL(CUSTOMER_PROFILE_CREATES_ENTRIES);
         sqLiteDatabase.execSQL(ADMIN_CATEGORY_DETAILS_ENTRIES);
+        sqLiteDatabase.execSQL(ADMIN_PRODUCT_DETAILS_ENTRIES);
 
 
     }
@@ -56,6 +69,7 @@ public class DBHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ CustomerMaster.Customers.TABLE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ CustomerMaster.Profile.TABLE_NAME);
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ CustomerMaster.ProductCategory.TABLE_NAME);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+ CustomerMaster.ProductItems.TABLE_NAME);
         onCreate(sqLiteDatabase);
     }
 
@@ -73,7 +87,7 @@ public class DBHelper extends SQLiteOpenHelper {
             return false;
         }
 
-            return true;
+        return true;
 
     }
 
@@ -88,11 +102,46 @@ public class DBHelper extends SQLiteOpenHelper {
             return false;
         }
 
-            return true;
+        return true;
 
 
     }
+    public ArrayList Retrive_admin_product_details(){
+        ArrayList<Products> list=new ArrayList<>();
+        SQLiteDatabase db=getReadableDatabase();
 
+        String sql="SELECT * FROM "+ CustomerMaster.ProductItems.TABLE_NAME;
+
+        Cursor cu=db.rawQuery(sql,null);
+        byte[] image;
+        String name;
+        String count;
+        String price;
+        String desc;
+        String id;
+        String fid;
+
+        while(cu.moveToNext()){
+
+            id=cu.getString(0);
+            name=cu.getString(1);
+            count=cu.getString(2);
+            desc=cu.getString(3);
+            price=cu.getString(4);
+            image=cu.getBlob(5);
+            fid=cu.getString(6);
+            Bitmap bitmap=null;
+
+            bitmap= BitmapFactory.decodeByteArray(image,0,image.length);
+
+            Products products=new Products(id,name,bitmap,fid,count,desc,price);
+            list.add(products);
+
+        }
+
+        return list;
+
+    }
     public ArrayList Retrive_Product_Category_Details(){
         ArrayList<CategoryItems> list=new ArrayList<>();
         SQLiteDatabase db=getReadableDatabase();
@@ -119,17 +168,17 @@ public class DBHelper extends SQLiteOpenHelper {
     }
     public boolean Admin_delete_category(String id){
 
-            try{
-                SQLiteDatabase db=getReadableDatabase();
-                String selection=CustomerMaster.ProductCategory.COLUMN_NAME_ID + " = ?";
-                String selectionArgs[]={id};
-                db.delete(CustomerMaster.ProductCategory.TABLE_NAME,selection,selectionArgs);
-                return true;
-            }
-            catch (Exception e){
-                e.printStackTrace();
-                return false;
-            }
+        try{
+            SQLiteDatabase db=getReadableDatabase();
+            String selection=CustomerMaster.ProductCategory.COLUMN_NAME_ID + " = ?";
+            String selectionArgs[]={id};
+            db.delete(CustomerMaster.ProductCategory.TABLE_NAME,selection,selectionArgs);
+            return true;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
     }
     public boolean Admin_add_Category_Details(byte[] image,String name){
         SQLiteDatabase db=getWritableDatabase();
@@ -141,10 +190,27 @@ public class DBHelper extends SQLiteOpenHelper {
             return false;
         }
 
-            return true;
+        return true;
 
     }
+    public boolean Admin_add_product_Details(String name,String desc,String price,String count,byte[] image,String fid){
+        SQLiteDatabase db=getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_PRODUCT_NAME,name);
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_COUNT,count);
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_DESCRIPTION,desc);
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_PRICE,price);
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_PRODUCTIMAGE,image);
+        values.put(CustomerMaster.ProductItems.COLUMN_NAME_FOREIGNKEY,fid);
 
+        long rowId=db.insert(CustomerMaster.ProductItems.TABLE_NAME,null,values);
+
+        if(rowId  == -1){
+            return false;
+        }
+
+        return true;
+    }
     public boolean Admin_update_category_Info(String id,String name,byte[]image){
         SQLiteDatabase db=getReadableDatabase();
         ContentValues values=new ContentValues();
@@ -164,9 +230,9 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     public Cursor  Customer_mail_check(String emailid){
-         SQLiteDatabase db=getReadableDatabase();
-         String projection[]={CustomerMaster.Customers.COLUMN_NAME_ID, CustomerMaster.Customers.COLUMN_NAME_NAME, CustomerMaster.Customers.COLUMN_NAME_EMAIL,
-          CustomerMaster.Customers.COLUMN_NAME_PASSWORD};
+        SQLiteDatabase db=getReadableDatabase();
+        String projection[]={CustomerMaster.Customers.COLUMN_NAME_ID, CustomerMaster.Customers.COLUMN_NAME_NAME, CustomerMaster.Customers.COLUMN_NAME_EMAIL,
+                CustomerMaster.Customers.COLUMN_NAME_PASSWORD};
 
         Cursor cursor=db.query(CustomerMaster.Customers.TABLE_NAME,projection, null,null,null,null,null);
         return  cursor;
@@ -181,22 +247,32 @@ public class DBHelper extends SQLiteOpenHelper {
         return cu;
     }
 
+    public Cursor Admin_Item_name_check(){
+        SQLiteDatabase db=getReadableDatabase();
+        String projection[]={CustomerMaster.ProductItems.COLUMN_NAME_ID, CustomerMaster.ProductItems.COLUMN_NAME_PRODUCT_NAME,
+                CustomerMaster.ProductItems.COLUMN_NAME_COUNT, CustomerMaster.ProductItems.COLUMN_NAME_DESCRIPTION, CustomerMaster.ProductItems.COLUMN_NAME_PRICE,
+                CustomerMaster.ProductItems.COLUMN_NAME_PRODUCTIMAGE, CustomerMaster.ProductItems.COLUMN_NAME_FOREIGNKEY};
+
+        Cursor cu=db.query(CustomerMaster.ProductItems.TABLE_NAME,projection,null,null,null,null,null);
+        return cu;
+    }
+
     public Bitmap getImage(String user_id){
         SQLiteDatabase db=getReadableDatabase();
         Bitmap bitmap=null;
-       // System.out.println("Bits :"+bitmap);
+        // System.out.println("Bits :"+bitmap);
         //String orderby= CustomerMaster.Profile.COLUMN_NAME_ID+" DESC";
         String sql="SELECT * FROM "+ CustomerMaster.Profile.TABLE_NAME+" WHERE "+ CustomerMaster.Profile.COLUMN_FOREIGNKEY_CUS_ID +"=?";
-       Cursor cu=db.rawQuery(sql,new String[]{user_id});
-      // System.out.println("Cus :"+user_id);
+        Cursor cu=db.rawQuery(sql,new String[]{user_id});
+        // System.out.println("Cus :"+user_id);
         byte[] image=null;
         int t=-1;
         while(cu.moveToNext()){
-           image=cu.getBlob(1);
+            image=cu.getBlob(1);
             t++;
             bitmap= BitmapFactory.decodeByteArray(image,0,image.length);
 
-           // System.out.println("Bit :"+user_id);
+            // System.out.println("Bit :"+user_id);
 
         }
         System.out.println("rrow :"+t);
