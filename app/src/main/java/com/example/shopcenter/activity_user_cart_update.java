79ef85bce9b6fd65c52e587df2cc3d0117ca6128
senthusj <senthu231997@gmail.com>
2019-09-21 11:00:57 +0530

    user_cart_update

---
+++ app/src/main/java/com/example/shopcenter/activity_user_cart_update.java
@@ -0,0 +1,14 @@
+package com.example.shopcenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_user_cart_update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_cart_update);
+    }
+}
